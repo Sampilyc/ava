94d44724850e3b03d610f918a17f0ed3268ec505
Sampilyc <167899192+Sampilyc@users.noreply.github.com>
2024-08-17 14:17:26 -0300

    Update agents-client-api.js

--- agents-client-api.js
+++ agents-client-api.js
@@ -518,5 +518,5 @@ agentsButton.onclick = async () => {
 }
 
 // Paste Your Created Agent and Chat IDs Here:
-agentId = "agt_GCH1h6Tq"
-chatId = "cht_8G6lq1XPnCcFN1P60layb"
+agentId = "agt_Kdj5kSgo"
+chatId = "cht_mXXZB3uz8fON41FRuWaQ5"
