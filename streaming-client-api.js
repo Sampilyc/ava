7684abc528fd55ebb762e8f11066694bdf7a072e
Sampilyc <167899192+Sampilyc@users.noreply.github.com>
2024-08-23 01:15:09 -0300

    Update streaming-client-api.js

--- streaming-client-api.js
+++ streaming-client-api.js
@@ -37,7 +37,7 @@ const streamEventLabel = document.getElementById('stream-event-label');
 
 const presenterInputByService = {
   talks: {
-    source_url: 'talk.jpg',
+    source_url: 'https://ava-swart.vercel.app/talk.jpg',
   },
   clips: {
     presenter_id: 'rian-lZC6MmWfC1',
